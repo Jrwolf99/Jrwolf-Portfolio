0ec1c0f49ede006735ba01ddb9c446379fcf28b0
jrwolf99 <jrwolf99@outlook.com>
2022-02-13 17:31:57 -0600

    new portfolio

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Website
+
+This website displays the projects that I have worked on and am working on.
---
+++ javascript/mobileMenu.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector(".mobile-nav__menu");
+const menuButton = document.querySelector(".mobile-nav__button");
+
+let menuOpen = false;
+
+menuButton.addEventListener('click', () => {
+
+    if (!menuOpen) {
+        menuOpen = true;
+        menuButton.classList.add('js-open');
+        menu.style.display = 'flex';
+        return;
+    }
+    menuButton.classList.remove('js-open');
+    menu.style.display = 'none';
+
+    menuOpen=false;
+   
+});
+    
